11276005e3dea0e7dae1a5d8035e60bdf17ce922
Yinan <yinan@teambition.com>
2015-05-24 22:02:12 +0800

    Add browser-sync test code.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
-var reload = browserSync.reload();
+var reload = browserSync.reload;
+var phantomjs = require('phantomjs');
 
 var path = {
   'jsapp': './app/**/*.ts',
-  'testapp': './test/spec/**/*.ts'
+  'jstest': './test/spec/**/*.js'
 };
 
 gulp.task('compile', function () {
@@ -16,20 +17,45 @@ gulp.task('compile', function () {
     .pipe($.typescript({
       out: 'app.js'
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('clean', function () {
-  return gulp.src('./dist')
+  return gulp.src(['./dist', './test/spec.js'])
     .pipe($.rimraf());
 });
 
 gulp.task('build-test', function () {
-  return gulp.src(path.testapp)
-    .pipe($.sourcemap.init())
+  return gulp.src(path.jstest)
+    .pipe($.sourcemaps.init())
     .pipe($.concat('spec.js'))
     .pipe(gulp.dest('./test/'));
 });
 
+gulp.task('test', ['build-test'], function () {
+  browserSync({
+    notify: false,
+    port: 9002,
+    server: {
+      baseDir: ['test', 'dist'],
+      routes: {
+        '/dist': 'dist',
+        '/bower_components': 'bower_components',
+      }
+    }
+  });
+  
+  gulp.watch('src/**/*.ts', ['compile']);
+  gulp.watch('test/spec/**/*.js', ['build-test']);
 
-gulp.task('default', $.sequence('clean', 'compile'));
+  // watch for changes
+  gulp.watch([
+    'test/*.html',
+    'src/**/*.js',
+    'test/spec/**/*.js'
+  ]).on('change', reload);
+
+});
+
+
+gulp.task('default', $.sequence('compile' ,'test'));
